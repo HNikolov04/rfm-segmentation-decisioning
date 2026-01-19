a2aa26a9da001ee1b03fda706cc5f39328d5adc1
Enis Ali <83497034+StormWarrior53@users.noreply.github.com>
2026-01-19 20:43:11 +0200

    Added rfm_model

---
+++ src/models/rfm_model.py
@@ -0,0 +1,9 @@
+from sklearn.preprocessing import StandardScaler
+from sklearn.cluster import KMeans
+
+def kmeans_segmentation(rfm_df, n_clusters=4):
+    scaler = StandardScaler()
+    rfm_scaled = scaler.fit_transform(rfm_df)
+    kmeans = KMeans(n_clusters=n_clusters, random_state=42)
+    rfm_df['Cluster'] = kmeans.fit_predict(rfm_scaled)
+    return rfm_df
